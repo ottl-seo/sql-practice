0a46e054fd7a1468bbcab8ea0b2fe3db8f3f2ca5
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-04-23 04:34:21 +0900

    e 수정

--- [pr2] subquery 연습하기/hw5.sql
+++ [pr2] subquery 연습하기/hw5.sql
@@ -3,7 +3,7 @@
 (예. 각 방법은 EIXSTS, IN, ALL, SOME 그리고 ANY와 같은 다른 집합 연산자들을 사용해야 하고 NOT을 같이 사용할 수도 있다) */
 
  -- a) 그랑프리에 가장 많이 참가한 레이싱 팀을 소유한 나라(country)를 찾아라.
-SELECT country FROM Constructors where races_entered IN (SELECT max(races_entered) from Constructors);
+SELECT country FROM Constructors where races_entered = ALL(SELECT max(races_entered) from Constructors);
 
 -- b) 그랑프리에서 적어도 한번은 우승을 한 드라이버가 소속된 레이싱 팀을 찾아라.
 SELECT constructor from Drivers as D where exists (SELECT * from Results as R where R.race_rank='first place' and R.driver=D.driver);
@@ -15,3 +15,5 @@ SELECT driver from Drivers where constructor = ANY(SELECT constructor from Const
 SELECT Race from Results where driver = SOME(SELECT driver from Drivers where constructor='Ferrari');
 
 -- e) 차체의 높이는 같으나 다른 팀의 차체보다 너비가 넓은 차체를 사용하는 드라이버들의 이름을 찾아라
+SELECT driver FROM Drivers WHERE constructor 
+IN (SELECT c1.constructor FROM Constructors c1, Constructors c2 WHERE c1.height=c2.height and c1.width>c2.width);
