8bbb9d9633c7179c7b67ffe491e2806aae75984b
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-04-27 22:20:01 +0900

    new

---
+++ sql-practice.sql
@@ -0,0 +1,6 @@
+
+EXECUTE sp_execute_external_script @language = N'Python'
+    , @script = N'OutputDataSet = InputDataSet'
+    , @input_data_1 = N'SELECT 1 AS hello'
+WITH RESULT SETS(([Hello World] INT));
+GO
