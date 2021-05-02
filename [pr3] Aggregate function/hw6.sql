2d337140b8c1e7fb9f5b8443274bf96bc6d1a21f
Yoonseo KIM <61778930+ottl-seo@users.noreply.github.com>
2021-05-02 20:34:19 +0900

    f, h만 고치자

--- [pr3] Aggregate function/hw6.sql
+++ [pr3] Aggregate function/hw6.sql
@@ -13,18 +13,30 @@ SELECT constructor, max(birthday) FROM Drivers GROUP BY constructor;
 SELECT avg(C.height) From Constructors C, Drivers D where D.country='German' and C.constructor=D.constructor;
 
 -- e) 그랑프리에서 우승을 한 적이 있는 레이싱 팀에 대해 각 레이싱 팀의 우승한 횟수를 구하여라.
-select constructor, count(constructor) as count from results 
-left outer join drivers on drivers.driver = results.driver where race_rank = 'first place' group by constructor;
+SELECT constructor, count(*) FROM Results INNER JOIN Drivers using(driver)
+    where race_rank = 'first place' GROUP BY constructor;
 
 -- f) 적어도 2 명의 드라이버를 가지고 있으면서 우승을 한 적이 있는 레이싱 팀의 수를 구하여라.
-select count(*) as count from (select count(*) from drivers as B 
-        left outer join (select distinct drivers.driver, constructor, race_rank from results 
-        left outer join drivers on drivers.driver = results.driver 
-        where race_rank ='first place') 
-    A on A.constructor = B.constructor  group by B.constructor having count(A.constructor)>1) C;
+/*
+
+with r1(constructor) as (SELECT constructor FROM Drivers GROUP BY constructor HAVING count(driver)>=2)
+with r2(constructor) as (SELECT D.constructor FROM Drivers D 
+        where exists (SELECT * FROM results R where race_rank='first place' and R.driver=D.driver))
+select * FROM r1 INNER JOIN r2 using(constructor);
+*/
+SELECT count(*) as count 
+FROM (select count(*) from drivers as d1 LEFT OUTER JOIN 
+(select distinct d2.driver, d2.constructor, r.race_rank from results r LEFT OUTER JOIN drivers d2
+on d2.driver = r.driver where r.race_rank ='first place') as A 
+on A.constructor = B.constructor  group by B.constructor having count(A.constructor)>1) C;
 
 -- g) 각 국가 별, 레이싱 팀에 속한 모든 드라이버의 평균 나이를 구하라. 한 국가에 속한 레이싱 팀이 하나 이상일 경우, 레이싱 팀별로 표시한다.
 -- hint) TIMESTAMPDIFF 함수 사용
-
+SELECT C.country, C.constructor, avg(TIMESTAMPDIFF(YEAR, D.birthday, '2021-01-01')) as avg_age
+FROM Drivers as D, Constructors as C where C.constructor=D.constructor
+GROUP BY C.country, C.constructor;
 
 -- h) 각 그랑프리 별, 두 명 이상의 선수가 순위에 든 레이싱 팀을 구하라.
+SELECT race, constructor, count(driver) 
+FROM Drivers INNER JOIN Results using(driver)
+where count(driver)>=2 GROUP BY race, constructor;
